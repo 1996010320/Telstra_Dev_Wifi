4f123a893ac79bcc1eaecf8c79364e94cb226b19
1996010320 <qiyongzhi@msn.com>
2016-06-16 13:32:37 +1000

    Add project files.

---
+++ Telstra_Dev_Wifi/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Telstra_Dev_Wifi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLatitude = new System.Windows.Forms.TextBox();
+            this.txtLongitude = new System.Windows.Forms.TextBox();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstHotspots = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(180, 135);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Latitude:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Longitude:";
+            // 
+            // txtLatitude
+            // 
+            this.txtLatitude.Location = new System.Drawing.Point(85, 31);
+            this.txtLatitude.Name = "txtLatitude";
+            this.txtLatitude.Size = new System.Drawing.Size(170, 20);
+            this.txtLatitude.TabIndex = 3;
+            // 
+            // txtLongitude
+            // 
+            this.txtLongitude.Location = new System.Drawing.Point(85, 62);
+            this.txtLongitude.Name = "txtLongitude";
+            this.txtLongitude.Size = new System.Drawing.Size(170, 20);
+            this.txtLongitude.TabIndex = 4;
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(85, 95);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(170, 20);
+            this.txtRadius.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Radius: ";
+            // 
+            // lstHotspots
+            // 
+            this.lstHotspots.FormattingEnabled = true;
+            this.lstHotspots.Location = new System.Drawing.Point(28, 189);
+            this.lstHotspots.Name = "lstHotspots";
+            this.lstHotspots.Size = new System.Drawing.Size(227, 134);
+            this.lstHotspots.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 348);
+            this.Controls.Add(this.lstHotspots);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLongitude);
+            this.Controls.Add(this.txtLatitude);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLatitude;
+        private System.Windows.Forms.TextBox txtLongitude;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstHotspots;
+    }
+}
+
---
+++ Telstra_Dev_Wifi/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Telstra_Dev_Wifi
+{
+    public partial class Form1 : Form
+    {
+        TelstraToken _token;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtLatitude.Text = "-37.81819";
+            txtLongitude.Text = "145.00176";
+            txtRadius.Text = "1000";
+        }
+
+        private async void btnCalculate_Click(object sender, EventArgs e)
+        {
+            List<TelstraWifiHotSpot> lst = new List<TelstraWifiHotSpot>();
+
+            if (_token == null || _token.ExpiredDt < DateTime.Now)
+            {
+                _token = await GetAccessToken("your consumer key", "your consumer secret");
+            }
+            
+
+            string latitude = txtLatitude.Text;
+            string longitude = txtLongitude.Text;
+            string radius = txtRadius.Text;
+
+            string url = string.Format("https://api.telstra.com/v1/wifi/hotspots?lat={0}&long={1}&radius={2}", latitude, longitude, radius);
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _token.AccessToken);
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url)
+            };
+
+            HttpResponseMessage responseMessage = await client.SendAsync(request);
+
+            var message = await responseMessage.Content.ReadAsStringAsync();
+
+            var jMsg = JsonConvert.DeserializeObject<object>(message);
+
+            
+
+            foreach (var j in (JArray)jMsg)
+            {
+                string wifiLatitude = ((JObject)j)["lat"].ToString();
+                string wifiLongitude = ((JObject)j)["long"].ToString();
+                string wifiAddress = ((JObject)j)["address"].ToString();
+                string wifiCity = ((JObject)j)["city"].ToString();
+                string wifiState = ((JObject)j)["state"].ToString();
+
+                TelstraWifiHotSpot hotspot = new TelstraWifiHotSpot(wifiLatitude, wifiLongitude, wifiAddress, wifiCity, wifiState);
+                lst.Add(hotspot);
+            }
+
+
+            if (lst.Count > 0)
+            {
+                lstHotspots.DataSource = lst;
+                lstHotspots.DisplayMember = lst.ToString();
+            }
+
+        }
+
+
+
+
+
+        internal class TelstraToken
+        {
+            internal string AccessToken;
+            internal DateTime ExpiredDt;
+
+            internal TelstraToken() { }
+
+            internal TelstraToken(string _accessToken, DateTime _expiredDt)
+            {
+                this.AccessToken = _accessToken;
+                this.ExpiredDt = _expiredDt;
+            }            
+        }
+
+        internal class TelstraWifiHotSpot
+        {
+            internal string latitude;
+            internal string longitude;
+            internal string address;
+            internal string city;
+            internal string state;
+
+            internal TelstraWifiHotSpot(string _latitude, string _longitude, string _address, string _city, string _state)
+            {
+                this.latitude = _latitude;
+                this.longitude = _longitude;
+                this.address = _address;
+                this.city = _city;
+                this.state = _state;
+            }
+
+
+            public override string ToString()
+            {
+                string fullAddress = string.IsNullOrWhiteSpace(address) ? string.Empty : address;
+                fullAddress += ", ";
+                fullAddress += string.IsNullOrWhiteSpace(city) ? string.Empty : city;
+                fullAddress += " ";
+                fullAddress += string.IsNullOrWhiteSpace(state) ? string.Empty : state;
+
+                return fullAddress;
+            }
+        }
+
+        private async Task<TelstraToken> GetAccessToken(string consumerkey, string consumersecret)
+        {
+            TelstraToken Token = new TelstraToken();
+
+            string AccessUrl = @"https://api.telstra.com/v1/oauth/token";
+
+            HttpClient authClient = new HttpClient();
+            HttpContent httpContent = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                {"client_id", consumerkey},
+                {"client_secret", consumersecret},
+                {"grant_type", "client_credentials"},
+                {"scope", "WIFI"}
+            });
+
+
+            HttpRequestMessage Request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(AccessUrl),
+                Content = httpContent
+            };
+
+            try
+            {
+                var ResponseMessage = await authClient.SendAsync(Request);
+                var Response = await ResponseMessage.Content.ReadAsStringAsync();
+
+                if (ResponseMessage.IsSuccessStatusCode)
+                {
+                    var AuthToken = JsonConvert.DeserializeObject<object>(Response);
+
+                    JObject jObj = JObject.Parse(AuthToken.ToString());
+
+                    Token.AccessToken = jObj["access_token"].ToString();
+                    Token.ExpiredDt = DateTime.Now.AddSeconds(double.Parse(jObj["expires_in"].ToString()));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return Token;
+        }
+        
+        
+    }
+}
